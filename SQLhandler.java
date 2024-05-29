1440576af316688d4c0f91dea80c43f24dee5ce5
Hoomammirza <hooman.mirzabozorg@gmail.com>
2024-05-29 11:12:08 +0330

    sqlconnect

---
+++ SQLhandler.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SQLhandler {
+    public static boolean isConnected = false;
+    public static void connect(){
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","honor1384");
+        } catch (Exception e){System.out.println(e);}
+    }
+}
