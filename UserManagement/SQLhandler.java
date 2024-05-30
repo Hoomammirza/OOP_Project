00ffae9d3ac15676a8f5d0a5255f16f8206e007d
Hoomammirza <hooman.mirzabozorg@gmail.com>
2024-05-30 10:59:05 +0330

    sqlconnect

--- UserManagement/SQLhandler.java
+++ UserManagement/SQLhandler.java
@@ -5,11 +5,17 @@ import java.sql.DriverManager;
 
 public class SQLhandler {
     public static boolean isConnected = false;
+    private static Connection con = null;
     public static void connect(){
-        Connection con = null;
         try {
+            if (isConnected)
+                throw new Exception("AlreadyConnected");
             Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","honor1384");
+            isConnected = true;
         } catch (Exception e){System.out.println(e);}
+    }
+    public static User readUser(String username,String password){
+        return null;
     }
 }
--- UserManagement/Users.java
+++ UserManagement/Users.java
@@ -1,5 +1,7 @@
 package UserManagement;
 
 public class Users {
-
+    public static User LoginUser = null;
+    //^user(\s+)create(\s+)-u(\s+)(?<username>.+)(\s+)-p(\s+)(?<password>.+)(\s+)(?<passwordcon>.+)(\s+)–email(\s+)(?<email>.+)(\s+)-n(\s+)(?<nickname>.+)(\s*)$
+    //private void canSignup(String usernam,String )
 }
