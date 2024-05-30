c373af4da7ed1997c7d044d615b829f4bbd04dc6
lenovo <arshia13848484@gmail.com>
2024-05-30 13:24:16 +0430

    append captcha

--- signup.java
+++ signup.java
@@ -1,6 +1,7 @@
 import UserManagement.User;
 import UserManagement.Users;
 
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
@@ -12,6 +13,116 @@ public class signup {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(input);
         return matcher;
+    }
+    public static ArrayList<ArrayList<String>> ascii_art=new ArrayList<ArrayList<String>>();
+    public static void madeAsciiArt()
+    {
+        ascii_art.add(new ArrayList<String>());
+        ascii_art.get(0).add(" .d8888b. ");
+        ascii_art.get(0).add("d88P  Y88b");
+        ascii_art.get(0).add("888    888");
+        ascii_art.get(0).add("888    888");
+        ascii_art.get(0).add("888    888");
+        ascii_art.get(0).add("888    888");
+        ascii_art.get(0).add("Y88b  d88P");
+        ascii_art.get(0).add("  Y8888P  ");
+        ascii_art.add(new ArrayList<String>());
+        ascii_art.get(1).add(" d888  ");
+        ascii_art.get(1).add("d8888  ");
+        ascii_art.get(1).add("  888  ");
+        ascii_art.get(1).add("  888  ");
+        ascii_art.get(1).add("  888  ");
+        ascii_art.get(1).add("  888  ");
+        ascii_art.get(1).add("  888  ");
+        ascii_art.get(1).add("8888888");
+        ascii_art.add(new ArrayList<String>());
+        ascii_art.get(2).add(" .d8888b. ");
+        ascii_art.get(2).add("d88P  Y88b");
+        ascii_art.get(2).add("       888");
+        ascii_art.get(2).add("     .d88P");
+        ascii_art.get(2).add(" .od888P\" ");
+        ascii_art.get(2).add("d88P\"     ");
+        ascii_art.get(2).add("888\"      ");
+        ascii_art.get(2).add("888888888 ");
+        ascii_art.add(new ArrayList<String>());
+        ascii_art.get(3).add(" .d8888b. ");
+        ascii_art.get(3).add("d88P  Y88b");
+        ascii_art.get(3).add("     .d88P");
+        ascii_art.get(3).add("    8888\" ");
+        ascii_art.get(3).add("     \"Y8b.");
+        ascii_art.get(3).add("888    888");
+        ascii_art.get(3).add("Y88b  d88P");
+        ascii_art.get(3).add(" \"Y8888P\" ");
+        ascii_art.add(new ArrayList<String>());
+        ascii_art.get(4).add("    d8888 ");
+        ascii_art.get(4).add("   d8P888 ");
+        ascii_art.get(4).add("  d8P 888 ");
+        ascii_art.get(4).add(" d8P  888 ");
+        ascii_art.get(4).add("d88   888 ");
+        ascii_art.get(4).add("8888888888");
+        ascii_art.get(4).add("      888 ");
+        ascii_art.get(4).add("      888 ");
+        ascii_art.add(new ArrayList<String>());
+        ascii_art.get(5).add("888888888 ");
+        ascii_art.get(5).add("888       ");
+        ascii_art.get(5).add("888       ");
+        ascii_art.get(5).add("8888888b. ");
+        ascii_art.get(5).add("     \"Y88b");
+        ascii_art.get(5).add("       888");
+        ascii_art.get(5).add("Y88b  d88P");
+        ascii_art.get(5).add(" \"Y8888P\" ");
+        ascii_art.add(new ArrayList<String>());
+        ascii_art.get(6).add(" .d8888b. ");
+        ascii_art.get(6).add("d88P  Y88b");
+        ascii_art.get(6).add("888       ");
+        ascii_art.get(6).add("888d888b. ");
+        ascii_art.get(6).add("888P \"Y88b");
+        ascii_art.get(6).add("888    888");
+        ascii_art.get(6).add("Y88b  d88P");
+        ascii_art.get(6).add(" \"Y8888P\" ");
+        ascii_art.add(new ArrayList<String>());
+        ascii_art.get(7).add("8888888888");
+        ascii_art.get(7).add("      d88P");
+        ascii_art.get(7).add("     d88P ");
+        ascii_art.get(7).add("    d88P  ");
+        ascii_art.get(7).add(" 88888888 ");
+        ascii_art.get(7).add("  d88P    ");
+        ascii_art.get(7).add(" d88P     ");
+        ascii_art.get(7).add("d88P      ");
+        ascii_art.add(new ArrayList<String>());
+        ascii_art.get(8).add(" .d8888b. ");
+        ascii_art.get(8).add("d88P  Y88b");
+        ascii_art.get(8).add("Y88b. d88P");
+        ascii_art.get(8).add(" \"Y88888\" ");
+        ascii_art.get(8).add(".d8P\"\"Y8b.");
+        ascii_art.get(8).add("888    888");
+        ascii_art.get(8).add("Y88b  d88P");
+        ascii_art.get(8).add(" \"Y8888P\" ");
+        ascii_art.add(new ArrayList<String>());
+        ascii_art.get(9).add(" .d8888b. ");
+        ascii_art.get(9).add("d88P  Y88b");
+        ascii_art.get(9).add("888    888");
+        ascii_art.get(9).add("Y88b. d888");
+        ascii_art.get(9).add(" \"Y888P888");
+        ascii_art.get(9).add("       888");
+        ascii_art.get(9).add("Y88b  d88P");
+        ascii_art.get(9).add(" \"Y8888P\" ");
+    }
+    public static ArrayList<String> showRandomCaptcha(String random)
+    {
+        ArrayList<String>answer=new ArrayList<String>();
+        for(int i=0;i<8;i++)
+        {
+            String a="";
+            for(int j=0;j<random.length();j++)
+            {
+                String b= String.valueOf(random.charAt(j));
+                a+=ascii_art.get(Integer.parseInt(b)).get(i);
+                a+="  ";
+            }
+            answer.add(a);
+        }
+        return answer;
     }
     private String username;
     private String password;
@@ -179,5 +290,4 @@ public class signup {
             input=scanner.nextLine();
         }
     }
-}
 }
