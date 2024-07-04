d51ea779e59507f734f04485d87af6893bb0ac1f
lenovo <arshia13848484@gmail.com>
2024-07-04 15:27:22 +0430

    update

--- MainMenu.java
+++ MainMenu.java
@@ -1,45 +1,58 @@
 import Misc.Misc;
+import UserManagement.User;
+import UserManagement.Users;
 
 import java.util.Scanner;
 import java.util.regex.Matcher;
 
 public class MainMenu {
+    public static User user;
     private static Scanner input = new Scanner(System.in);
     public static int run(){
+        user=Users.LoginUser;
         String in;
         boolean quit = false;
 
-        Matcher login;
-        Matcher ForgotPassword;
-        Matcher exit;
-        Matcher showcurrrentmenu;
+        Matcher startGame;
+        Matcher showCard;
+        Matcher HistoryGame;
+        Matcher ProfileMenu;
+        Matcher exitAccount;
+        Matcher showMenu;
 
         System.out.println("commands:\n" +
-                "*  user login -u <username> -p <password>\n" +
-                "*  forgot my password -u <username>\n" +
-                "*  show current menu\n" +
-                "*  exit");
+                "* start game\n"+
+                "* show my cards:\n" +
+                "* show history game\n" +
+                "* show current menu\n" +
+                "* go to shop menu\n"+
+                "* go to profile menu"+
+                "* exit account");
 
         while (!quit){
             in = input.nextLine();
-            login = Misc.getMatcher(in,"^user(\\s+)login(\\s+)-u(\\s+)(?<username>\\S+)(\\s+)-p(\\s+)(?<password>\\S+)(\\s*)$");
-            ForgotPassword = Misc.getMatcher(in,"^forgot(\\s+)my(\\s+)password(\\s+)-u(\\s+)(?<username>\\S+)$");
-            exit = Misc.getMatcher(in, "^exit(\\s*)$");
-            showcurrrentmenu = Misc.getMatcher(in, "^show current menu(\\s*)$");
+            startGame = Misc.getMatcher(in,"start game");
+            showCard = Misc.getMatcher(in,"show my cards:");
+            HistoryGame = Misc.getMatcher(in, "show history game");
+            ProfileMenu = Misc.getMatcher(in, "go to profile menu");
+            showMenu=Misc.getMatcher(in,"show current menu");
+            showMenu=Misc.getMatcher(in,"go to shop menu");
+            exitAccount=Misc.getMatcher(in,"exit account");
+            if (startGame.find()){
 
-            if (login.find()){
-                if (login(login))
-                    return 4;
-            } else if (ForgotPassword.find()){
-                forgotpassword(ForgotPassword);
-            }else if (showcurrrentmenu.find()) {
-                System.out.println("signin");
-            }else if (exit.find()) {
-                return 0;
             }
+            else if (showCard.find()){
+
+            }
+            else if (exitAccount.find()) {
+                return 1;
+            }
+            else if(showMenu.find())
+            {
+                System.out.println("Main Menu");
+            }
+
         }
         return 0;
     }
-
-
 }
